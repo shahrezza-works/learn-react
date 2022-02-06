4d60112cd26b0f64bf3daa5bb7043ee53cf9d5d5
shahrezza-works <shahrezza.works@gmail.com>
2022-02-06 20:56:26 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LEARNING REACT
+
+Here is the repo on learning react for my personal reference
---
+++ first-react/index.js
@@ -0,0 +1,24 @@
+ReactDOM.render(<div>
+    <h1 className="header">Hi, my name is Shah Rezza!</h1>
+    <ul>
+        <li>My Hobbies is coding</li>
+        <li>I like to eat on my free time</li>
+    </ul>
+    </div>
+    , 
+
+    document.getElementById("root"))
+
+/* 
+Imperative method
+
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child of the div#root
+
+    
+*/
